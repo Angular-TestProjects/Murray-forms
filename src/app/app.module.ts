bde04d6bfbccf2677283ef304a199ff8d3fe8bf6
Bill.Avramenko <billavramenko@gmail.com>
2023-05-24 18:43:56 +0300

    New components were added to the main NgModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {SidebarItemComponent} from "./sidebar/sidebar-item.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    SidebarComponent,
+    SidebarItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
