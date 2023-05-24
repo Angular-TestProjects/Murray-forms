a81701b94919ae954cfe423e58dfef2e36e64eb2
Bill.Avramenko <billavramenko@gmail.com>
2023-05-24 18:42:45 +0300

    Were added some base routes and application section description model.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppSection} from "./app-section.model";
+import {MainPageComponent} from "./main-page/main-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: "full", redirectTo: 'main'},
+  {path: 'main', component: MainPageComponent},
+  {path: '**', redirectTo: 'main' }
+];
+
+const appSections: AppSection[] = [
+  { label: "Intro", name: "Root", path: "", component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: "AppSections", useValue: appSections}
+  ]
 })
 export class AppRoutingModule { }
