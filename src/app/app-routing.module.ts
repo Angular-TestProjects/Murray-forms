80c1054c223c88589fdce93d30810739b5a24b8f
Bill.Avramenko <billavramenko@gmail.com>
2023-06-05 14:45:10 +0300

    A new route has been added, called reactive-driven.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 import {AppSection} from "./app-section.model";
 import {MainPageComponent} from "./main-page/main-page.component";
 import {TemplateDrivenComponent} from "./template-driven/template-driven.component";
+import {ReactiveWithBuilderComponent} from "./reactive-with-builder/reactive-with-builder.component";
 
 const routes: Routes = [
   {path: '', pathMatch: "full", redirectTo: 'main'},
   {path: 'main', component: MainPageComponent},
   {path: 'template-driven', component: TemplateDrivenComponent},
+  {path: 'reactive-driven', component: ReactiveWithBuilderComponent},
   {path: '**', redirectTo: 'main' }
 ];
 
 const appSections: AppSection[] = [
   { label: "Intro", name: "Root", path: "", component: MainPageComponent },
-  { label: "Template driven form", name: "TemplateDriven", path: "template-driven", component: TemplateDrivenComponent }
+  { label: "Template driven form", name: "TemplateDriven", path: "template-driven", component: TemplateDrivenComponent },
+  { label: "Template driven form", name: "TemplateDriven", path: "template-driven", component: TemplateDrivenComponent },
+  { label: "Reactive driven form", name: "ReactiveDriven", path: "reactive-driven", component: ReactiveWithBuilderComponent }
 ];
 
 @NgModule({
