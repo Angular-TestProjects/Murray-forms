c1d65739b6bdebec0837c6c5adda7c3ff7d0d5a3
Bill.Avramenko <billavramenko@gmail.com>
2023-06-05 22:05:34 +0300

    A new route was added for the validation component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,22 @@ import {AppSection} from "./app-section.model";
 import {MainPageComponent} from "./main-page/main-page.component";
 import {TemplateDrivenComponent} from "./template-driven/template-driven.component";
 import {ReactiveWithBuilderComponent} from "./reactive-with-builder/reactive-with-builder.component";
+import {ValidationApproachesComponent} from "./validation-approaches/validation-approaches.component";
 
 const routes: Routes = [
   {path: '', pathMatch: "full", redirectTo: 'main'},
   {path: 'main', component: MainPageComponent},
   {path: 'template-driven', component: TemplateDrivenComponent},
   {path: 'reactive-driven', component: ReactiveWithBuilderComponent},
+  {path: 'validators', component: ValidationApproachesComponent},
   {path: '**', redirectTo: 'main' }
 ];
 
 const appSections: AppSection[] = [
   { label: "Intro", name: "Root", path: "", component: MainPageComponent },
   { label: "Template driven form", name: "TemplateDriven", path: "template-driven", component: TemplateDrivenComponent },
-  { label: "Reactive driven form", name: "ReactiveDriven", path: "reactive-driven", component: ReactiveWithBuilderComponent }
+  { label: "Reactive driven form", name: "ReactiveDriven", path: "reactive-driven", component: ReactiveWithBuilderComponent },
+  { label: "Reactive driven form with validators", name: "ReactiveDrivenWithValidators", path: "validators", component: ValidationApproachesComponent }
 ];
 
 @NgModule({
@@ -27,4 +30,4 @@ const appSections: AppSection[] = [
     {provide: "AppSections", useValue: appSections}
   ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{}
