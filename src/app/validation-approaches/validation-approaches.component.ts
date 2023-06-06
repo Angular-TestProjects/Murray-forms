7aa87e04b7de10dcc55374a29fd7b288ec17ecc4
Bill.Avramenko <billavramenko@gmail.com>
2023-06-06 15:50:41 +0300

    Various types of validation were added.

--- src/app/validation-approaches/validation-approaches.component.ts
+++ src/app/validation-approaches/validation-approaches.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-validation-approaches',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class ValidationApproachesComponent {
 
+  @ViewChild('skuToast', { static: true }) skuToast!: ElementRef;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      sku: ["", Validators.compose([Validators.required, ValidationApproachesComponent.skuValidator])]
+    })
+  }
+
+  onSubmit(value: string) {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      $.toast({
+        class: 'error',
+        message: `Please submit any SKU value.`
+      });
+      return;
+    }
+
+    $(this.skuToast.nativeElement).toast();
+
+    console.log('you submitted value:', value);
+  }
+
+  static skuValidator(control: FormControl): { [s: string]: boolean } | null {
+    if (control.value.match(/^123/)) {
+      return null;
+    }
+
+    return {invalidSku: true};
+  }
+
+  resetForm() {
+    this.form.controls['sku'].patchValue("EMPTY");
+  }
 }
